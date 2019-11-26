1ffc5aaa2c316c82e2cd72c6894663bd22b8b977
Fran <fmjerez@icloud.com>
2019-11-26 19:58:11 +0100

    Minutes & Seconds done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -3,33 +3,22 @@ class Chronometer {
     this.currentTime = 0,
       this.intervalId = 0
   }
-  startClick(printTime) {
+  startClick() {
     this.intervalId = setInterval(() => {
-      printTime()
       this.currentTime++
     }, 1000)
   }
   
   getMinutes() {
-   return Math.floor(this.currentTime/60)  
+    return Math.floor(this.currentTime/60)
   }
+
   getSeconds() {
-    return this.currentTime-this.getMinutes()*60
-  }
-  twoDigitsNumber(value) {
-    if(value<10){
-     return ('0' + value).slice(-2)
-    }else return value.toString()
-  }
-  stopClick() {
-    clearInterval(this.intervalId)
-  }
-  resetClick() {
-    this.currentTime = 0
-    return this.currentTime
-  }
-  splitClick() {
-    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`
+    if(this.currentTime > 60){
+      return this.currentTime = this.currentTime - 60
+    }
+    return Math.floor(this.currentTime)
+
   }
 }
 
