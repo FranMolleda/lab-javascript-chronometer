9ea1a3de4fd8a4632d720d058a128dc54b8e88b2
Fran <fmjerez@icloud.com>
2019-11-27 11:32:47 +0100

    twoDigits done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,38 +1,34 @@
-class Chronometer {
-  constructor() {
+class Chronometer{
+  constructor(){
     this.currentTime = 0,
       this.intervalId = 0
   }
-  startClick(printTime) {
+  startClick() {
     this.intervalId = setInterval(() => {
-      printTime()
       this.currentTime++
     }, 1000)
   }
   
   getMinutes() {
-   return Math.floor(this.currentTime/60)  
+    return Math.floor(this.currentTime/60)
   }
+
   getSeconds() {
-    return this.currentTime-this.getMinutes()*60
+    if(this.currentTime > 60){
+      return this.currentTime = this.currentTime - 60
+    }
+    return Math.floor(this.currentTime)
   }
-  twoDigitsNumber(value) {
-    if(value<10){
-     return ('0' + value).slice(-2)
-    }else return value.toString()
-  }
-  stopClick() {
-    clearInterval(this.intervalId)
-  }
-  resetClick() {
-    this.currentTime = 0
-    return this.currentTime
-  }
-  splitClick() {
-    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`
-  }
+
+  twoDigitsNumber (number){
+    if(number<10){
+      return ('0' + number).slice(-2)
+    }
+    return number.toString()
 }
 
+
+}
 
 
 
